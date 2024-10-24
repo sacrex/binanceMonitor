d25e6664aab2069c12d3bc839337049bfcbfa7b6
MaoLiangxian <1029193287@qq.com>
2024-10-24 20:44:38 +0800

    fix: run.sh

--- run.sh
+++ run.sh
@@ -6,6 +6,7 @@ HTTP_PROXY=http://192.168.112.1:7890
 CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags '-extldflags "-static"' -o monitor.exe ./
 
 mv monitor.exe release/
+cp .env release/
 ./release/monitor.exe
 
 # 服务查看
